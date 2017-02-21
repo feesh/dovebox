720f4599ee4273b5fc7630e5482a3398b257b863
Trish Ang <trish@counsyl.com>
2017-02-21 08:34:36 -0800

    Header section component

---
+++ src/components/header-section.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Search from './search-section.js';
+
+class Header extends React.Component {
+  render() {
+    const { doves } = this.props;
+
+    return (
+      <header>
+        <div className="content">
+          <div className="title">
+            <h1><a href="" title="DoveBox">🐥📦</a></h1>
+          </div>
+          <Search doves={doves} />
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
