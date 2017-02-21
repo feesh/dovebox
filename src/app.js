d48476f0af33d3572dc31a66ea3e6b0a1ddedd5f
Trish Ang <trish@counsyl.com>
2017-02-21 08:33:47 -0800

    Adding header in

--- src/app.js
+++ src/app.js
@@ -2,26 +2,23 @@ import 'whatwg-fetch';
 import React from 'react';
 
 import DoveTable from './components/dovetable-component.js';
+import Header from './components/header-section.js';
 
 /*  Gallery viewer app */
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-    };
-  }
-
 
   render() {
     const { doves } = this.props;
 
     return (
-      <section className="gallery">
-        <div className="content">
-          <DoveTable doves={doves} />
-        </div>
-      </section>
+      <div className="page_wrapper">
+        <Header doves={doves} />
+        <section className="gallery">
+          <div className="content">
+            <DoveTable doves={doves} />
+          </div>
+        </section>
+      </div>
     );
   }
 }
